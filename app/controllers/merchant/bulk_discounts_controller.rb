87c16c7af5cefc6fc8300a9c53e34eb284cb224c
awosborn <alanturing@example.coclear>
2021-01-15 19:46:02 -0700

    Working bulk discount show

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -4,7 +4,7 @@ class Merchant::BulkDiscountsController < ApplicationController
   end
   
   def show
-    
+    @discount = BulkDiscount.find(params[:id])
   end
 
   def new
---
+++ spec/features/merchant/bulk_discounts/show_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'as a merchant', type: :feature do
+  describe 'when I visit my bulk disocunt show page' do
+    before(:each) do
+    @merchant1 = Merchant.create!(name: 'Hair Care')
+
+    @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+    @customer_2 = Customer.create!(first_name: 'Cecilia', last_name: 'Jones')
+    @customer_3 = Customer.create!(first_name: 'Mariah', last_name: 'Carrey')
+    @customer_4 = Customer.create!(first_name: 'Leigh Ann', last_name: 'Bron')
+
+    @invoice_1 = Invoice.create!(merchant_id: @merchant1.id, customer_id: @customer_1.id, status: 2)
+    @invoice_2 = Invoice.create!(merchant_id: @merchant1.id, customer_id: @customer_1.id, status: 2)
+    @invoice_3 = Invoice.create!(merchant_id: @merchant1.id, customer_id: @customer_2.id, status: 2)
+    @invoice_4 = Invoice.create!(merchant_id: @merchant1.id, customer_id: @customer_3.id, status: 2)
+    
+
+    @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id)
+    @item_2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: @merchant1.id)
+    @item_3 = Item.create!(name: "Brush", description: "This takes out tangles", unit_price: 5, merchant_id: @merchant1.id)
+    @item_4 = Item.create!(name: "Hair tie", description: "This holds up your hair", unit_price: 1, merchant_id: @merchant1.id)
+
+    @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 0)
+    @ii_2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 1, unit_price: 8, status: 0)
+    @ii_3 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 2)
+    @ii_4 = InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_4.id, quantity: 1, unit_price: 5, status: 1)
+
+    @transaction1 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_1.id)
+    @transaction2 = Transaction.create!(credit_card_number: 230948, result: 1, invoice_id: @invoice_3.id)
+    @transaction3 = Transaction.create!(credit_card_number: 234092, result: 1, invoice_id: @invoice_4.id)
+    
+    @bulk_discount1 = BulkDiscount.create!(percentage_discount: 20, quantity_threshold: 15, merchant_id: @merchant1.id, item_id: @item_1.id)
+    @bulk_discount2 = BulkDiscount.create!(percentage_discount: 30, quantity_threshold: 30, merchant_id: @merchant1.id, item_id: @item_4.id)
+    end
+
+    it 'When I visit my bulk discount show page I see the bulk discounts quantity and price' do
+      # require 'pry'; binding.pry
+      visit "merchant/#{@merchant1.id}/bulk_discounts/#{@bulk_discount1.id}"
+
+      expect(page).to have_content(@bulk_discount1.percentage_discount)
+      expect(page).to have_content(@bulk_discount1.quantity_threshold)
+
+      
+    end
+  end
+end

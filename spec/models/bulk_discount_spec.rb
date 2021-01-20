ad0d571944787fa8c4aa2516d5a02bae1da40ca7
Isabelle Villasenor <villasenori@outlook.com>
2021-01-20 08:03:28 -0700

    Add Model Spec for Discount_Price

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -10,4 +10,29 @@ describe BulkDiscount do
   describe 'relationships' do
     it { should belong_to :merchant }
   end
+  
+  describe 'instance methods' do
+    describe "#discount_price" do
+      it 'should return the discounted price if a discount is applied' do
+        @m1 = Merchant.create!(name: 'Merchant 1')
+    
+        @c1 = Customer.create!(first_name: 'Yo', last_name: 'Yoz', address: '123 Heyyo', city: 'Whoville', state: 'CO', zip: 12345)
+    
+        @i1 = Invoice.create!(merchant_id: @m1.id, customer_id: @c1.id, status: 2, created_at: '2012-03-25 09:54:09')
+    
+        @item_2 = Item.create!(name: 'rest', description: 'dont test me', unit_price: 12, merchant_id: @m1.id)
+       
+        @ii_3 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 87, unit_price: 12, status: 2)
+    
+        @t1 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @i1.id)
+        
+        expect(@i1.total_revenue).to eq(1044)
+        
+        @d1 = @m1.bulk_discounts.create!(discount: 20.0, quantity:30)
+    
+        expect(@i1.total_revenue.round(2)).to eq(835.2)
+      end
+    end
+  end
+
 end
